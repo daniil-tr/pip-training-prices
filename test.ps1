60aec534b7da991a84960f3851ffe25ef3747e92
daniil-tr <daniil.tr@hotmail.com>
2019-05-24 16:18:18 +0300

    Updated test.ps1

--- test.ps1
+++ test.ps1
@@ -13,9 +13,12 @@ try {
     # Workaround to remove dangling images
     docker-compose -f ./docker/docker-compose.test.yml down
 
-    ##docker build -f docker/Dockerfile.test -t ${IMAGE} .
-    docker-compose -f ./docker/docker-compose.test.yml up --build --abort-on-container-exit --exit-code-from test
+    docker-compose -f ./docker/docker-compose.test.yml up --abort-on-container-exit --exit-code-from test
+
+    $unitTestsExitCode = $LastExitCode
 } finally {
     # Workaround to remove dangling images
     docker-compose -f ./docker/docker-compose.test.yml down
 }
+
+exit $unitTestsExitCode
